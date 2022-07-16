77f1a144a3ca5433443ff5c271e68aa5b7d5dc65
Divyanshu Bhoyar <divyanshubhoyar@yahoo.com>
2022-07-16 12:39:54 +0530

    fronend with trpc/next

---
+++ src/pages/_app.tsx
@@ -0,0 +1,66 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next'
+import { loggerLink } from '@trpc/client/links/loggerLink'
+import { httpBatchLink } from '@trpc/client/links/httpBatchLink'
+import superjson from 'superjson'
+
+import { AppRouter } from '../server/route/app.router'
+import { url } from '../constants'
+import { trpc } from '../utils/trpc'
+import { UserContextProvider } from '../context/user.context'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const { data, error, isLoading } = trpc.useQuery(['users.me'])
+
+  if (isLoading) {
+    return <>Loading user...</>
+  }
+
+  return (
+    <UserContextProvider value={data}>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </UserContextProvider>
+  )
+}
+
+// * configuring trpc with nextjs frontend
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    
+    // use links feature to optimize performance
+    const links = [
+      loggerLink(),
+      httpBatchLink({
+        maxBatchSize: 10,
+        url,
+      }),
+    ]
+    // react-query engine config
+    return {
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            staleTime: 800,
+          },
+        },
+      },
+      
+      headers() {
+        if (ctx?.req) {
+          return {
+            ...ctx.req.headers,
+            'x-ssr': '1',
+          }
+        }
+        return {}
+      },
+      
+      links,
+      transformer: superjson,
+    }
+  },
+  ssr: false,
+})(MyApp)
---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,17 @@
+import * as trpcNext from "@trpc/server/adapters/next";
+
+import { createContext } from "../../../server/createContext";
+import { appRouter } from "../../../server/route/app.router";
+
+// plug next api routes adapter
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext,
+  onError({ error }) {
+    if (error.code === "INTERNAL_SERVER_ERROR") {
+      console.error("Something went wrong", error);
+    } else {
+      console.error(error);
+    }
+  },
+});
